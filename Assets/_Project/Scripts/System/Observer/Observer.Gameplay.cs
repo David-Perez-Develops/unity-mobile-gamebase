7e422f415d8229ae0b4e6af99a3785b279235d99
David-Perez-Develops <84375494+David-Perez-Develops@users.noreply.github.com>
2026-08-18 18:01:57 +0200

    refactor: remove sample level events from core

--- Assets/_Project/Scripts/System/Observer/Observer.Gameplay.cs
+++ Assets/_Project/Scripts/System/Observer/Observer.Gameplay.cs
@@ -2,11 +2,6 @@ using System;
 
 public static partial class Observer
 {
-    public static Action<Level> StartLevel;
-    public static Action<Level> ReplayLevel;
-    public static Action<Level> SkipLevel;
-    public static Action<Level> WinLevel;
-    public static Action<Level> LoseLevel;
-
+    // Shop/inventory notification retained until the economy pass generalizes it.
     public static Action<string> EquipPlayerSkin;
 }
